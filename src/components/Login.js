68dfa90b99be61858b1a5059ea017cf2f771d661
NVerma11 <nikitaverma706@gmail.com>
2024-04-26 20:12:08 +0530

    React-Netflix

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <>
+        <Header/>
+        <div className=''>
+            <img className="bg-no-repeat"
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/c7f07b68-7989-4ff7-a31e-11c17dcc2fea/fcf685b8-3f9f-42d8-9af3-4bb86fa5a3b8/IN-en-20240422-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background-img"></img>
+        </div>
+    </>
+  )
+}
+
+export default Login
